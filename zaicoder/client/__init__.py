fc3bd8479ef24b186df2e70e86d1ae077fcff50f
cvsz <seaza@msn.com>
2026-07-15 04:41:53 +0700

    feat(client): export live stream transport

--- zaicoder/client/__init__.py
+++ zaicoder/client/__init__.py
@@ -2,15 +2,18 @@
 
 from .api import ProductAPIClient
 from .config import ClientConfig
+from .live_stream import CancellationSignal, ProductAPIStreamTransport
 from .retries import RetryPolicy
 from .streaming import EventStreamParser, StreamProtocolError, parse_event_chunks
 from .transport import ProductAPIError, ProductAPITransport, TransportResponse
 
 __all__ = [
+    "CancellationSignal",
     "ClientConfig",
     "EventStreamParser",
     "ProductAPIClient",
     "ProductAPIError",
+    "ProductAPIStreamTransport",
     "ProductAPITransport",
     "RetryPolicy",
     "StreamProtocolError",
