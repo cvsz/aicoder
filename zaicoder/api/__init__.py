28869e3e3bb62b3a4fcf3b3cb1033aaa447279f1
cvsz <seaza@msn.com>
2026-07-15 04:06:43 +0700

    feat(api): export authentication middleware

--- zaicoder/api/__init__.py
+++ zaicoder/api/__init__.py
@@ -1,13 +1,18 @@
 """Versioned provider-neutral Product API."""
 
 from .application import ProductAPIApplication, ProductAPIRequest, ProductAPIResponse
+from .auth import AuthMiddleware, Principal, StaticTokenAuthenticator, TokenRecord
 from .openapi import build_openapi_schema
 from .wsgi import ProductAPIWSGI
 
 __all__ = [
+    "AuthMiddleware",
+    "Principal",
     "ProductAPIApplication",
     "ProductAPIRequest",
     "ProductAPIResponse",
     "ProductAPIWSGI",
+    "StaticTokenAuthenticator",
+    "TokenRecord",
     "build_openapi_schema",
 ]
